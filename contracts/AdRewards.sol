bd19b9d3e67754daf80f699b2151bb696c2f4d18
MyBlokchainGuru <119838455+MyBlokchainGuru@users.noreply.github.com>
2022-12-09 18:54:32 -0600

    Create AdRewards.sol

---
+++ contracts/AdRewards.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/math/SafeMath.sol";
+import "./AdRewardsToken.sol";
+
+contract AdRewards {
+  using SafeMath for uint256;
+
+  address public owner;
+  AdRewardsToken public token;
+  mapping(address => uint256) public rewards;
+
+  constructor(address tokenAddress) public {
+    owner = msg.sender;
+    token = AdRewardsToken(tokenAddress);
+  }
+
+  function sign(bytes32 transaction) public view returns (bytes memory) {
+    return keccak256(abi.encodePacked(transaction, msg.sender));
+  }
+
+  function claimRewards(
+    string memory name,
+    string memory address,
+    address wallet
+  ) public payable {
+    require(rewards[msg.sender] > 0, "You don't have any rewards to claim.");
+    require(verifySignature(sign(name), wallet), "Invalid signature.");
+
+    rewards[msg.sender] = rewards[msg.sender].sub(1);
+    token.transfer(wallet, 1);
+  }
+
+  function verifySignature(bytes memory sig, address signer) internal view returns (bool) {
+    return address(uint(keccak256(abi.encodePacked(sig)))).eq(signer);
+  }
+}
